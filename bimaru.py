a104555d56416c371a3c7db6577c59ca41c22e6f
Sofia Paiva <2003sasp@gmail.com>
2023-05-16 10:41:28 +0100

    no real changes

--- bimaru.py
+++ bimaru.py
@@ -11,55 +11,55 @@ import sys
 from sys import stdin
 
 from search import (
-    Problem,
-    Node,
-    astar_search,
-    breadth_first_tree_search,
-    depth_first_tree_search,
-    greedy_search,
-    recursive_best_first_search,
+   Problem,
+   Node,
+   astar_search,
+   breadth_first_tree_search,
+   depth_first_tree_search,
+   greedy_search,
+   recursive_best_first_search,
 )
 
 
 class BimaruState:
-    state_id = 0
+   state_id = 0
 
-    def __init__(self, board):
-        self.board = board
-        self.id = BimaruState.state_id
-        BimaruState.state_id += 1
+   def __init__(self, board):
+      self.board = board
+      self.id = BimaruState.state_id
+      BimaruState.state_id += 1
 
-    def __lt__(self, other):
-        return self.id < other.id
+   def __lt__(self, other):
+      return self.id < other.id
 
-    # TODO: outros metodos da classe
+   # TODO: outros metodos da classe
 
 
 class Board:
-    """Representação interna de um tabuleiro de Bimaru."""
+   """Representação interna de um tabuleiro de Bimaru."""
 
-    def __init__(self, board: np.ndarray):
-        """Construtor da classe"""
-        self.board = board
+   def __init__(self, board: np.ndarray):
+      """Construtor da classe"""
+      self.board = board
 
 
-    def get_value(self, row: int, col: int) -> str:
-        """Devolve o valor na respetiva posição do tabuleiro."""
+   def get_value(self, row: int, col: int) -> str:
+      """Devolve o valor na respetiva posição do tabuleiro."""
 
-        return self.board[row][col]
+      return self.board[row][col]
 
-    def adjacent_vertical_values(self, row: int, col: int) -> tuple:#(str, str) estava a dar erro
-        """Devolve os valores imediatamente acima e abaixo,
-        respectivamente."""
-        return self.board[row - 1][col], self.board[row + 1][col]
+   def adjacent_vertical_values(self, row: int, col: int) -> tuple:#(str, str) estava a dar erro
+      """Devolve os valores imediatamente acima e abaixo,
+      respectivamente."""
+      return self.board[row - 1][col], self.board[row + 1][col]
 
-    def adjacent_horizontal_values(self, row: int, col: int) -> tuple:
-        """Devolve os valores imediatamente à esquerda e à direita,
-        respectivamente."""
-        return self.board[row][col - 1], self.board[row][col + 1]
+   def adjacent_horizontal_values(self, row: int, col: int) -> tuple:
+      """Devolve os valores imediatamente à esquerda e à direita,
+      respectivamente."""
+      return self.board[row][col - 1], self.board[row][col + 1]
 
-    @staticmethod
-    def parse_instance():
+   @staticmethod
+   def parse_instance():
       """Lê o test do standard input (stdin) que é passado como argumento
       e retorna uma instância da classe Board.
       formato do input:
@@ -89,43 +89,43 @@ class Board:
          board[hint_row][hint_column] = hint
       return Board(board)
 
-    # TODO: outros metodos da classe
+      # TODO: outros metodos da classe
 
 
 class Bimaru(Problem):
-    def __init__(self, board: Board):
-        """O construtor especifica o estado inicial."""
-        self.initial = BimaruState(board)
-        # TODO
-        pass
+   def __init__(self, board: Board):
+      """O construtor especifica o estado inicial."""
+      self.initial = BimaruState(board)
+      # TODO
+      pass
 
-    def actions(self, state: BimaruState):
-        """Retorna uma lista de ações que podem ser executadas a
-        partir do estado passado como argumento."""
-        # TODO
-        pass
+   def actions(self, state: BimaruState):
+      """Retorna uma lista de ações que podem ser executadas a
+      partir do estado passado como argumento."""
+      # TODO
+      pass
 
-    def result(self, state: BimaruState, action):
-        """Retorna o estado resultante de executar a 'action' sobre
-        'state' passado como argumento. A ação a executar deve ser uma
-        das presentes na lista obtida pela execução de
-        self.actions(state)."""
-        # TODO
-        pass
+   def result(self, state: BimaruState, action):
+      """Retorna o estado resultante de executar a 'action' sobre
+      'state' passado como argumento. A ação a executar deve ser uma
+      das presentes na lista obtida pela execução de
+      self.actions(state)."""
+      # TODO
+      pass
 
-    def goal_test(self, state: BimaruState):
-        """Retorna True se e só se o estado passado como argumento é
-        um estado objetivo. Deve verificar se todas as posições do tabuleiro
-        estão preenchidas de acordo com as regras do problema."""
-        # TODO
-        pass
+   def goal_test(self, state: BimaruState):
+      """Retorna True se e só se o estado passado como argumento é
+      um estado objetivo. Deve verificar se todas as posições do tabuleiro
+      estão preenchidas de acordo com as regras do problema."""
+      # TODO
+      pass
 
-    def h(self, node: Node):
-        """Função heuristica utilizada para a procura A*."""
-        # TODO
-        pass
+   def h(self, node: Node):
+      """Função heuristica utilizada para a procura A*."""
+      # TODO
+      pass
 
-    # TODO: outros metodos da classe
+   # TODO: outros metodos da classe
 
 
 if __name__ == "__main__":
