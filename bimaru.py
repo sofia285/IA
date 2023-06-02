bfbbf9ea42d793b736a3f86ac689e64d247ce3e5
Sofia Paiva <2003sasp@gmail.com>
2023-06-02 20:26:15 +0100

    gives correct output

--- bimaru.py
+++ bimaru.py
@@ -8,7 +8,7 @@
 
 WATER = 0
 BOAT = 1
-EMPTY = 1
+EMPTY = 2
 
 CIRCLE = 'c'
 TOP = 't'
@@ -44,12 +44,6 @@ class BimaruState:
       self.board = board
       self.id = BimaruState.state_id
       BimaruState.state_id += 1
-      self.num_boats ={
-         'couracado': 0,
-         'cruzador': 0,
-         'contratorpecidos': 0,
-         'submarino': 0,
-      }
 
    def __lt__(self, other):
       return self.id < other.id
@@ -69,91 +63,154 @@ class Board:
    def __init__(self, board: np.ndarray):
       """Construtor da classe"""
       self.board = board
+      self.num_boats ={
+         'couracado': 0,
+         'cruzador': 0,
+         'contratorpecidos': 0,
+         'submarino': 0,
+      }
 
 
-   def get_value(self, row: int, col: int) -> str:
+   def get_value(self, dim: int ,row: int, col: int) -> int:
       """Devolve o valor na respetiva posição do tabuleiro."""
 
-      return self.board[row][col]
+      return self.board[dim][row][col]
+   
+   def set_value(self, dim: int, row: int, col: int, value: str):
+      """Atribui o valor 'value' à posição especificada pelos argumentos 'row' e 'col'."""
+      self.board[dim][row][col] = value
 
-   def adjacent_vertical_values(self, row: int, col: int) -> tuple:
+   def adjacent_vertical_values(self, dim: int, row: int, col: int) -> tuple:
       """Devolve os valores imediatamente acima e abaixo,
       respectivamente."""
       if row == 0:
-         return None, self.get_value(row + 1, col)
+         return None, self.get_value(dim, row + 1, col)
       elif row == 9:
-         return self.get_value(row - 1, col), None
+         return self.get_value(dim, row - 1, col), None
       else:
-         return self.get_value(row - 1, col), self.get_value(row + 1, col)
+         return self.get_value(dim, row - 1, col), self.get_value(dim, row + 1, col)
 
-   def adjacent_horizontal_values(self, row: int, col: int) -> tuple:
+   def adjacent_horizontal_values(self, dim: int, row: int, col: int) -> tuple:
       """Devolve os valores imediatamente à esquerda e à direita,
       respectivamente."""
       if col == 0:
-         return None, self.get_value(row, col + 1)
+         return None, self.get_value(dim, row, col + 1)
       elif col == 9:
-         return self.get_value(row, col - 1), None
+         return self.get_value(dim, row, col - 1), None
       else:
-         return self.get_value(row, col - 1), self.get_value(row, col + 1)
+         return self.get_value(dim, row, col - 1), self.get_value(dim, row, col + 1)
       
-
-   def get_row(self, dim: int, row: int) -> np.ndarray:
-      """Devolve a linha especificada pelo argumento 'row'."""
-      return self.board[dim, row]
+   def adjacent_diagonal_top_values(self, dim: int, row: int, col: int) -> tuple:
+      """Devolve os valores imediatamente nas diagonais superiores"""
+      if row == 0: 
+         return None, None
+      elif col == 0:
+         return None, self.get_value(dim, row - 1, col + 1)
+      elif col == 9:
+            return self.get_value(dim, row - 1, col - 1), None
+      else:
+         return self.get_value(dim, row - 1, col - 1), self.get_value(dim, row - 1, col + 1)
    
-   def get_column(self,dim: int, col: int) -> np.ndarray:
-      """Devolve a coluna especificada pelo argumento 'col'."""
-      return self.board[dim, :, col]
+   def adjacent_diagonal_bottom_values(self, dim: int, row: int, col: int) -> tuple:
+      """Devolve os valores imediatamente nas diagonais inferiores"""
+      if row == 9: 
+         return None, None
+      elif col == 0:
+         return None, self.get_value(dim, row + 1, col + 1)
+      elif col == 9:
+            return self.get_value(dim, row + 1, col - 1), None
+      else:
+         return self.get_value(dim, row + 1, col - 1), self.get_value(dim, row + 1, col + 1)
+
+   def get_empty_spaces_count(self):
+      """Devolve se existe algum espaço vazio no tabuleiro."""
+      return np.any(self.board[1] == 1)
+
+   def has_been_visited(self, dim: int, row: int, col: int) -> bool:
+      """Devolve se a posição já foi visitada."""
+      return self.get_value(dim, row, col) == 1
+
+   def print_board(self, original_board):
+      """Imprime o tabuleiro."""
+      boats_pad = np.pad(self.board[0], 1)
+      boats_adjacents = boats_pad[0:10, 1:11] + 2 * boats_pad[1:11, 1:11] + 4 * boats_pad[2:12, 1:11] + 8 * boats_pad[1:11, 0:10] + 16 * boats_pad[1:11, 2:12]
+
+      line = ''
+      for i in range (10):
+         for j in range (10):
+            if original_board[i][j] != '':
+               line+= original_board[i][j];
+               continue;
+            if boats_adjacents[i][j] == 2:
+               line += CIRCLE
+            elif boats_adjacents[i][j] == 3:
+               line += BOTTOM
+            elif boats_adjacents[i][j] == 6:
+               line += TOP
+            elif boats_adjacents[i][j] == 10:
+               line += RIGHT
+            elif boats_adjacents[i][j] == 18:
+               line += LEFT
+            elif boats_adjacents[i][j] == 7 or boats_adjacents[i][j] == 26:
+               line += MIDDLE
+            else:
+               line += '.'
+         line += '\n'
+         
+      print(line)
+   
+   def get_empty_spaces_count(self):
+      """Devolve se existe algum espaço vazio no tabuleiro."""
+      return np.any(board[1] == 1)
+
+   def fill_water_boats(self) ->bool:
+      """Preenche as posições que só podem ter água ou barcos."""
+      diff = True
+      while diff:
+         #sums the rows and columns of the first two matrices
+         empy_n_boat_row_sums = np.sum(self.board[:2], axis = 2)
+         empy_n_boat_col_sums = np.sum(self.board[:2], axis = 1)
+
+         #subtracts the sum of the rows and columns from the third matrix
+         empty_n_boat_rows_diff = np.subtract(empy_n_boat_row_sums[0], self.board[2, :, 9])
+         rows_diff = np.subtract(np.add(empy_n_boat_row_sums[0], empy_n_boat_row_sums[1]), self.board[2, :, 9])
+         empty_n_boat_cols_diff = np.subtract(empy_n_boat_col_sums[0], self.board[2, :, 0])
+         cols_diff = np.subtract(np.add(empy_n_boat_col_sums[0], empy_n_boat_col_sums[1]), self.board[2, :, 0])
+
+         #finds the indices of the rows that are equal to zero
+         rows_equal_to_zero_indices_1 = np.where(empty_n_boat_rows_diff == 0)[0]
+         rows_equal_to_zero_indices_2 = np.where(rows_diff == 0)[0]
+         rows_zeros_indices = np.where(np.all(self.board[1] == 0, axis = 1))[0]
+         rows_indices_1 = np.setdiff1d(rows_equal_to_zero_indices_1, rows_zeros_indices)
+         rows_indices_2 = np.setdiff1d(rows_equal_to_zero_indices_2, rows_zeros_indices)
+
+         #finds the indices of the columns that are equal to zero
+         cols_equal_to_zero_indices_1 = np.where(empty_n_boat_cols_diff == 0)[0]
+         cols_equal_to_zero_indices_2 = np.where(cols_diff == 0)[0]
+         cols_zeros_indices = np.where(np.all(self.board[1] == 0, axis = 0))[0]
+         cols_indices_1 = np.setdiff1d(cols_equal_to_zero_indices_1, cols_zeros_indices)
+         cols_indices_2 = np.setdiff1d(cols_equal_to_zero_indices_2, cols_zeros_indices)
+
+         #checks if there are any differences
+         diff = np.size(rows_indices_2) != 0 or np.size(rows_indices_1) != 0 or np.size(cols_indices_2) != 0 or np.size(cols_indices_1) != 0
+         
+         #puts boats in rows
+         for k in rows_indices_2:
+            self.board[0, k, :] = np.add(self.board[1, k, :], self.board[0, k, :])
+            self.board[1, k, :] = np.zeros(self.board.shape[2])
+
+         #puts water in rows
+         for i in rows_indices_1:
+            self.board[1, i, :] = np.zeros(self.board.shape[2])
+         
+         #puts boats in columns
+         for l in cols_indices_2:
+            self.board[0, :, l] = np.add(self.board[1, :, l], self.board[0, :, l])
+            self.board[1, :, l] = np.zeros(self.board.shape[1])
       
-   def set_value(self, dim: int, row: int, col: int, value: str):
-      """Atribui o valor 'value' à posição especificada pelos
-      argumentos 'row' e 'col'."""
-      self.board[dim][row][col] = value
-
-   def get_col_count(self, col: int) -> int:
-     """Deveolve a contagem da coluna especificada pelo argumento 'col'."""
-     return int(self.board[10][col])
-   
-   def get_row_count(self, row: int) -> int:
-       """Devevolve a contagem da linha especificada pelo argumento 'row'."""
-       return int(self.board[row][10])
-
-   def loop_fill_water(self):
-      """chama a função fill_water até que não haja alterações em nenhuma das matrizes"""
-      original_board = np.copy(self.board)  # Make a copy of the original matrix
-      while True:
-         self.fill_water_boats()
-         if not np.array_equal(self.board, original_board):
-            original_board = np.copy(self.board)  # Update the original matrix
-         else:
-            break
-
-
-   def fill_water_boats(self):
-      """Preenche as posições que só podem ter água"""
-
-      #sums the rows and columns of the first two matrices
-      row_sums = np.sum(self.board[0], axis = 1)
-      col_sums = np.sum(self.board[0], axis = 0)
-
-      #subtracts the sum of the rows and columns from the third matrix
-      rows_diff = np.subtract(row_sums, self.board[2, :, 9])
-      cols_diff = np.subtract(col_sums, self.board[2, :, 0])
-
-      #finds the indices of the rows and columns that are equal to zero
-      rows_equal_to_zero_indices = np.where(rows_diff == 0)[0]
-      rows_zeros_indices = np.where(np.all(self.board[1] == 0, axis = 1))[0]
-      rows_indices = np.setdiff1d(rows_equal_to_zero_indices, rows_zeros_indices)
-
-      cols_equal_to_zero_indices = np.where(cols_diff == 0)[0]
-      cols_zeros_indices = np.where(np.all(self.board[1] == 0, axis = 0))[0]
-      cols_indices = np.setdiff1d(cols_equal_to_zero_indices, cols_zeros_indices)
-
-      for i in rows_indices:
-         self.board[1, i, :] = np.zeros(self.board.shape[2])
-
-      for j in cols_indices:
-         self.board[1, :, j] = np.zeros(self.board.shape[1])
+         #puts water in columns
+         for j in cols_indices_1:
+            self.board[1, :, j] = np.zeros(self.board.shape[1])
 
    @staticmethod
    def parse_instance():
@@ -594,7 +651,8 @@ class Board:
 class Bimaru(Problem):
    def __init__(self, board: Board):
       """O construtor especifica o estado inicial."""
-      self.initial = BimaruState(board)
+      state = BimaruState(board)
+      super().__init__(state)
 
    def actions(self, state: BimaruState):
       """Retorna uma lista de ações que podem ser executadas a
@@ -610,32 +668,27 @@ class Bimaru(Problem):
       # TODO
       pass
 
-   def goal_test(self, state: BimaruState) -> bool:
+   def goal_test(self, state: BimaruState):
       """Retorna True se e só se o estado passado como argumento é
       um estado objetivo. Deve verificar se todas as posições do tabuleiro
       estão preenchidas de acordo com as regras do problema."""
-      # Check if any position is empty (contains zero)
-      if np.any(state.board[1] == 1):
-        return False
-      else:
-         return True
+      return state.board.get_empty_spaces_count()
 
    def h(self, node: Node):
       """Função heuristica utilizada para a procura A*."""
-      # TODO
-      pass
-
-   # TODO: outros metodos da classe
+      board = node.state.board
+      return board.count_pos_with_two_actions
 
 
 if __name__ == "__main__":
 
-   # TODO:
    # Ler o ficheiro do standard input,
+   original_board, board = Board.parse_instance()
    # Usar uma técnica de procura para resolver a instância,
+   board.fill_water_boats()
+   board_state = BimaruState(board)
+   #print(board.board)
    # Retirar a solução a partir do nó resultante,
    # Imprimir para o standard output no formato indicado.
-   original_board, board = Board.parse_instance()
-   board.loop_fill_water()
-   print(board.board)
+   board.print_board(original_board.board)
    
